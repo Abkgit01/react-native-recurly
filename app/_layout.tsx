416f3fe0dec8806c7ed43ddde2b15bbccee23312
Abubakar <bappaiya520@gmail.com>
2026-07-23 12:55:24 +0100

    Load app fonts and clean home links

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,28 @@
 import { Stack } from "expo-router";
 import "@/global.css";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack screenOptions={{headerShown: false}} />;
+  const [fontsLoaded] = useFonts({
+    "sans-regular": require("../assets/assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "sans-bold": require("../assets/assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "sans-medium": require("../assets/assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "sans-semibold": require("../assets/assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    "sans-extrabold": require("../assets/assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "sans-light": require("../assets/assets/fonts/PlusJakartaSans-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
